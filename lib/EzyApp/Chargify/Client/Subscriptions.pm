a459b80175772f4ce9a08442bf0f579420ce0e52
Jason Galea <jason@lecstor.com>
2014-12-13 22:44:15 +1000

    rename methods, add components method and docs

--- lib/EzyApp/Chargify/Client/Subscriptions.pm
+++ lib/EzyApp/Chargify/Client/Subscriptions.pm
@@ -5,9 +5,16 @@ with
   'EzyApp::Chargify::Role::Client',
   'EzyApp::Chargify::Role::ClientBasicAuth';
 
-=item
+=header SYNOPSIS
 
-  $subscriptions->fetch($page, $per_page);
+  $subs = EzyApp::Chargify::Client::Subscriptions->new(
+    api_key => 'abc123',
+    api_host => 'subdomain.example.com',
+    ua_inactivity_timeout => 120,
+    debug_on => 1,
+  );
+
+  $subscriptions->list($page, $per_page);
 
   Returns the json response body
 
@@ -99,56 +106,58 @@ with
 
 =cut
 
-sub fetch{
-  my ($self, $page, $per_page) = @_;
+=item single
 
-  my $url = $self->fetch_url($page, $per_page);
-
-  my $res;
-  foreach my $try (1..3){
-    $res = $self->try_fetch($url, $try);
-    last if $res;
-    $self->debug($res);
-    if ($try < 3){
-      $self->debug("Subscriptions Fetch: sleep for 30 then try again..");
-      sleep(30);
+  $subscriptions->single($id, sub{
+    my ($err, $data) = @_;
+    if ($err){
+      if ($err->{code}){
+        warn "$err->{code} response: $err->{message}";
+      } else {
+        warn "Connection error: $err->{message}";
+      }
     } else {
-      $self->debug("Subscriptions Fetch: monumental fail!\n");
+      warn Dumper $data;
     }
-  }
+  })
 
-  $self->response($res);
+  my $data;
+  try{ $data = $subscriptions->single($id) }
+  catch { die $_ };
 
-  return $res->json();
+=cut
+
+sub single{
+  my ($self, $id, $callback) = @_;
+  my $url = $self->base_url. sprintf "/subscriptions/$id.json";
+  return $self->_request('get', $url, $callback);
 }
 
-=item fetch_url
+=item list
 
-  URL: https://<subdomain>.chargify.com/subscriptions.<format>
-  Method: GET
-  Optional Parameters:
   page: an integer value which specifies which page of results to fetch, starting at 1. Fetching successively higher page numbers will return additional results, until there are no more results to return (in which case an empty result set will be returned). Defaults to 1.
   per_page: how many records to fetch in each request, defaults to 20. The maximum allowed is 200 – any per_page value over 200 will be changed to 200.
 
 =cut
 
-sub fetch_url{
-  my ($self, $page, $per_page) = @_;
+sub list{
+  my ($self, $page, $per_page, $callback) = @_;
   $page ||= 1;
   $per_page ||= 200;
-  my $url = $self->base_url;
-  $url .= sprintf '/subscriptions.json?page=%s&per_page=%s', $page, $per_page;
-  $self->debug("Subscriptions fetch url: $url");
-  return $url;
+  my $url = $self->base_url. sprintf '/subscriptions.json?page=%s&per_page=%s', $page, $per_page;
+  return $self->_request('get', $url, $callback);
 }
 
-sub try_fetch{
-  my ($self, $url) = @_;
-  my $res = $self->user_agent->get($url)->res;
-  $self->debug("Subscriptions Fetch Message: ".$res->message."\n");
-  return $res if lc($res->message) eq 'ok';
-  return;
+=item components
+
+=cut
+
+sub components{
+  my ($self, $id, $callback) = @_;
+  my $url = $self->base_url. sprintf "/subscriptions/$id/components.json";
+  return $self->_request('get', $url, $callback);
 }
+
 
 
 no Moose;
